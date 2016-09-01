1e06fbb07c7586e8c9ea1c5e1267aef47197ed0b
ganeshredcobra <ganeshredcobra@gmail.com>
2016-09-01 23:50:00 +0530

    Chronos timer added

--- warpaffine/main.cpp
+++ warpaffine/main.cpp
@@ -1,6 +1,8 @@
 #include "opencv2/opencv.hpp"
 
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
 
 using namespace cv;
 using namespace std;
@@ -15,6 +17,11 @@ int main(int, char**)
     Mat frame;
     Mat cropimage,imgRotated;
     int angle = 0;
+
+    typedef std::chrono::high_resolution_clock Time;
+    typedef std::chrono::milliseconds ms;
+    typedef std::chrono::duration<float> fsec;
+
     for(;;)
     {
         clock_t tic = clock();
@@ -22,7 +29,13 @@ int main(int, char**)
         cap >> frame; // get a new frame from camera
         cropimage = frame;
         RotateMatrix = getRotationMatrix2D(Point2f(cropimage.cols/2, cropimage.rows/2), angle, 1.0);
+        auto t0 = Time::now();
         warpAffine(cropimage, imgRotated, RotateMatrix, cropimage.size(), cv::INTER_LINEAR);
+        auto t1 = Time::now();
+        fsec fs = t1 - t0;
+        ms d = std::chrono::duration_cast<ms>(fs);
+        //std::cout << fs.count() << "s\n";
+        std::cout << d.count() << "ms\n";
         imshow("Image Rotated",imgRotated);
         angle++;
         if(angle == 360)
